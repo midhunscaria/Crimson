57f7e2d0fd329788fe554304327877391d74b21f
Vimal James <vj6978@gmail.com>
2018-11-04 17:42:30 +0000

    Budget

--- Crimson/app/src/main/java/com/example/crimson/crimson/goals_fragment.java
+++ Crimson/app/src/main/java/com/example/crimson/crimson/goals_fragment.java
@@ -1,108 +1,42 @@
 package com.example.crimson.crimson;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link goals_fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link goals_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class goals_fragment extends Fragment {
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    private OnFragmentInteractionListener mListener;
-//
-//    public goals_fragment() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment goals_fragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static goals_fragment newInstance(String param1, String param2) {
-//        goals_fragment fragment = new goals_fragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
+    public View parentHolder;
+    public EditText goalTarget;
+    public EditText goalAmount;
+    public EditText goalPeriod;
+    public Button creategoal;
+
+}
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goals_fragment, container, false);
+      parentHolder=inflater.inflate(R.layout.fragment_dues_fragment, container, false);
+      goalTarget=(EditText)parentHolder.findViewById(R.id.goalsTarget);
+      goalAmount=(EditText)parentHolder.findViewById(R.id.goalsAmount);
+      goalPeriod=(EditText)parentHolder.findViewById(R.id.goalsPeriod);
+      creategoal=(Button) parentHolder.findViewById(R.id.createGoals);
+
+      creategoal.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+
+
+          }
+      });
+
+        return parentHolder;
     }
 //
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
 //
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-//
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
-}
